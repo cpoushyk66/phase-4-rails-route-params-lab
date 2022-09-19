2f9d94bdbb88dcfa39e0120ede26fe8f334bbe2f
Camilla Poushyk <camilla.poushyk@gmail.com>
2022-09-19 17:35:13 -0400

    commit tree 983b0f502a3b763bf3365e77488e929bef8cf38b

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,28 @@
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
+    # all the instances of a class
+    if params[:name]
+      students = Student.select{|student| student.first_name.downcase == params[:name].downcase || student.last_name.downcase == params[:name].downcase}
+    else
+      students = Student.all
+   end
     render json: students
+  end
+
+  def show
+    student = Student.find(params[:id])
+    render json: student
+  end
+
+  def grades
+    students = Student.order(:grade).reverse_order
+    render json: students
+  end
+
+  def highest_grade
+    student = Student.order(:grade).last
+    render json: student
   end
 
 end
